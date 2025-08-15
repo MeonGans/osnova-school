55ef528462b2d5731375fd317b98da3b913ab228
Meon Gans <meon.gans@gmail.com>
2025-08-15 19:16:54 +0300

    add '/' to api.php 2

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,11 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', function () {
-    return response()->json(['status' => 'ok']);
+    return response()->json([
+        'status' => 'ok',
+        'app'    => config('app.name'),
+        'laravel'=> app()->version(),
+    ]);
 });
 Route::get('news', [\App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
 Route::get('news/{id}', [\App\Http\Controllers\NewsController::class, 'show'])->name('news.show');
